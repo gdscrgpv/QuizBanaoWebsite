1b790a1cbc958984ebe992ecd2979723370b516b
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-04 20:49:05 +0530

    Imported Support in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Support from "./components/Support";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Header />
         <Features />
+        <Support />
       </main>
       <Footer />
     </>
